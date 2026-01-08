9e9f9aef3d17676588015abf6434e4c197a2f939
Сергей Зиновьев <sergejzinovev833@gmail.com>
2026-01-09 00:27:46 +0800

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
 const API_KEY = "05125fd6-df9e-4262-b007-93b670896319";
-const BASE_URL = "https://exam-api-courses.std-900.ist.mospolytech.ru";
+const BASE_URL = "http://exam-api-courses.std-900.ist.mospolytech.ru";
